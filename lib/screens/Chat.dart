c9e2a5f2246cae2529badc94e8523fe4ff1fd71e
SoulNinja-dev <aadhi123awesome@gmail.com>
2020-12-27 00:15:10 +0530

    finished the home screen will come back to work :)

--- lib/screens/Chat.dart
+++ lib/screens/Chat.dart
@@ -1,12 +1,63 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class Chat extends StatelessWidget {
+  final List<String> names = [
+    "SoulNinja",
+    "SKP",
+    "Rohit",
+    "PHP devs",
+    "Rize",
+    "Felis",
+    "Dhruv",
+    "Chintu"
+  ];
+  final List<String> message = [
+    "50daysofwindows",
+    "mobile developer",
+    "vim user",
+    "react dev",
+    "xd--",
+    "xml is better than flutter",
+    "Bot dev",
+    "I am whitehat jr son"
+  ];
+  final List<String> time = [
+    "11:11",
+    "14:34",
+    "00:00",
+    "05:50",
+    "69:96",
+    "06:44",
+    "42:00",
+    "07:23",
+    "03:03"
+  ];
+  final List colors = [
+    Colors.blue,
+    Colors.green,
+    Colors.black,
+    Colors.teal,
+    Colors.grey,
+    Colors.red,
+    Colors.yellow,
+    Colors.indigoAccent,
+    Colors.blueGrey
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: double.infinity,
-      width: double.infinity,
-      child: Text('chat'),
-    );
+    return Scaffold(body: ListView.builder(
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: CircleAvatar(backgroundColor: colors[index]),
+          title: Text(
+            names[index],
+          ),
+          subtitle: Text(
+            message[index],
+          ),
+        );
+      },
+    ));
   }
 }
